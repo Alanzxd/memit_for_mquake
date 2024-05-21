1ea217fcf8e8dc0cfd02b2269154162fefd0d07c
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2024-05-21 12:19:48 +0800

    Add files via upload

---
+++ dsets/__init__.py
@@ -0,0 +1,6 @@
+from .attr_snippets import AttributeSnippets
+from .counterfact import CounterFactDataset, MultiCounterFactDataset
+from .knowns import KnownsDataset
+from .tfidf_stats import get_tfidf_vectorizer
+from .zsre import MENDQADataset
+from .mquake_CF_3k import MQuAKEDataset_CF_3k  # 添加这行来导入你的新数据集类
---
+++ dsets/mquake_CF_3k.py
@@ -0,0 +1,36 @@
+import json
+import typing
+from pathlib import Path
+
+import torch
+from torch.utils.data import Dataset
+
+from util.globals import *
+
+REMOTE_ROOT = f"{REMOTE_ROOT_URL}/data/dsets"
+
+class MQuAKEDataset_CF_3k(Dataset):
+    """
+    Dataset class for loading MQuAKE-CF data.
+    """
+    def __init__(self, data_dir: str, size: typing.Optional[int] = None, *args, **kwargs):
+        data_dir = Path(data_dir)
+        mquake_loc = data_dir / "MQuAKE-CF-3k.json"
+        if not mquake_loc.exists():
+            remote_url = f"{REMOTE_ROOT}/MQuAKE-CF-3k.json"
+            print(f"{mquake_loc} does not exist. Downloading from {remote_url}")
+            data_dir.mkdir(exist_ok=True, parents=True)
+            torch.hub.download_url_to_file(remote_url, mquake_loc)
+        
+        with open(mquake_loc, "r") as f:
+            self.data = json.load(f)
+        if size is not None:
+            self.data = self.data[:size]
+        
+        print(f"Loaded MQuAKE-CF dataset with {len(self)} elements")
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        return self.data[item]

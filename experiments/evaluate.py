9aad9a14df8145e9f7f5bf5bd894f96f3920128f
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2024-06-20 16:32:28 +0800

    Update evaluate.py

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -269,7 +269,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--model_name",
-        choices=["gpt2-medium", "gpt2-large", "gpt2-xl", "EleutherAI/gpt-j-6B", "EleutherAI/gpt-j-6B", "vicuna-7b"],
+        choices=["gpt2-medium", "gpt2-large", "gpt2-xl", "EleutherAI/gpt-j-6B", "EleutherAI/gpt-j-6B", "vicuna-7b-delta-v0"],
         default="gpt2-xl",
         help="Model to edit.",
         required=True,

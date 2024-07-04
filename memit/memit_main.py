0521d34f98b92fac50a8068e8481a21b62598b2b
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2024-07-04 19:03:26 +0800

    Update memit_main.py

--- memit/memit_main.py
+++ memit/memit_main.py
@@ -102,6 +102,7 @@ def execute_memit(
 
     for request in requests:
         # Retrieve k/v pair if already stored in cache
+        request['target_new']['str'] += tok.eos_token
         cache_fname = (
             Path(
                 str(cache_template).format(

b7b312ba250e1ccc8481e8185e43bc97ddf528f6
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2024-07-05 00:27:02 +0800

    Update generate.py

--- util/generate.py
+++ util/generate.py
@@ -147,22 +147,12 @@ def generate_fast(
 
             cur_context = slice(cur_context.stop, cur_context.stop + 1)
 
-    txt = [tok.decode(x) for x in input_ids.detach().cpu().numpy().tolist()]
 
-    cleaned_txt = []
-    for original_prompt, generated_text in zip(prompts, txt):
-        prompt_with_eos = original_prompt + eos_token
-        if generated_text.startswith(prompt_with_eos):
-            # 删除第一个EOS token
-            generated_text = generated_text[len(prompt_with_eos):].strip()
-        cleaned_txt.append(generated_text)
-    
+    txt = [tok.decode(x) for x in input_ids.detach().cpu().numpy().tolist()]
     txt = [
         unicodedata.normalize("NFKD", x)
-        .split(eos_token)[0]  # 这里使用EOS token分割
+        .split("<|endoftext|>")[0]
         .replace("\n\n", " ")
-        .replace("", "")
-        for x in cleaned_txt
+        .replace("<|endoftext|>", "")
+        for x in txt
     ]
-    
-    return txt

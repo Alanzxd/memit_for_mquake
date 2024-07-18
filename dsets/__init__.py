98248d6ee6961d3164c39547aeb4b3cadc18b8f2
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2024-07-18 19:29:13 +0800

    Update __init__.py

--- dsets/__init__.py
+++ dsets/__init__.py
@@ -3,7 +3,11 @@ from .counterfact import CounterFactDataset, MultiCounterFactDataset
 from .knowns import KnownsDataset
 from .tfidf_stats import get_tfidf_vectorizer
 from .zsre import MENDQADataset
-from .mquake_CF_3k import MQuAKEDataset_CF_3k  # 添加这行来导入你的新数据集类
-from .MQuAKE_T import MQuAKE_T  # 添加这行来导入你的新数据集类
-from .mquake_2002 import MQuAKE_2002 # 添加这行来导入你的新数据集类
-from .MQuAKE_hard import MQuAKE_hard  # 添加这行来导入你的新数据集类
+from .mquake_CF_3k import MQuAKEDataset_CF_3k  
+from .MQuAKE_T import MQuAKE_T  
+from .mquake_2002 import MQuAKE_2002 
+from .MQuAKE_hard import MQuAKE_hard  
+from .mquake_CF_3k_A import MQuAKEDataset_CF_3k_A  
+from .MQuAKE_T_A import MQuAKE_T_A  
+from .mquake_2002_A import MQuAKE_2002_A 
+from .MQuAKE_hard_A import MQuAKE_hard_A  
